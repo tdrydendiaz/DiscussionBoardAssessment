d621beb3306f3644b89d146491b8bccacca98cc9
tdrydendiaz <tdrydendiaz@gmail.com>
2019-07-26 09:23:02 +0100

    server

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const _=require("lodash");
+
+
+let mongoose = require("mongoose");
+
+
+
+mongoose.connect(
+    'mongodb://localhost:27017/example',
+    { useNewUrlParser: true }
+).then(
+    () => { console.log("success") },
+    (err) => { /* handle errors */ }
+
+);
+
+//username
+//const name = require("./routes/name");
+
+
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+//app.use("/name", name);
+
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`server running on port ${port}`));
